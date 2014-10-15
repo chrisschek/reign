13a4b64f8bca711ead9738ac9cfe80a038df3c3e
Yen Pai <ypai@kompany.org>
2014-10-15 01:45:28 -0700

    using local instead of in-process ZK to test disconnects manually

--- src/test/java/io/reign/examples/ReignServer.java
+++ src/test/java/io/reign/examples/ReignServer.java
@@ -6,19 +6,19 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ReignServer {
-	private static final Logger logger = LoggerFactory.getLogger(ReignServer.class);
+    private static final Logger logger = LoggerFactory.getLogger(ReignServer.class);
 
-	public static void main(String[] args) throws Exception {
-		/** init and start reign using builder **/
-		Reign reign = Reign.maker().zkConnectString("localhost:22181").zkTestServerPort(22181).startZkTestServer(true)
-		        .get();
-		reign.start();
+    public static void main(String[] args) throws Exception {
+        /** init and start reign using builder **/
+        Reign reign = Reign.maker().zkConnectString("localhost:2181").zkTestServerPort(22181).startZkTestServer(false)
+                .get();
+        reign.start();
 
-		/** let server run **/
-		Object obj = new Object();
-		synchronized (obj) {
-			obj.wait();
-		}
+        /** let server run **/
+        Object obj = new Object();
+        synchronized (obj) {
+            obj.wait();
+        }
 
-	}
+    }
 }
