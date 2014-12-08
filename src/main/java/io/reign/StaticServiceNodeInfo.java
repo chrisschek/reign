b20afa5451d06dbaf02e97ab3a528cf1ea5b7a66
Yen Pai <ypai@kompany.org>
2014-12-07 22:11:30 -0800

    source formatting

--- src/main/java/io/reign/StaticServiceNodeInfo.java
+++ src/main/java/io/reign/StaticServiceNodeInfo.java
@@ -25,78 +25,84 @@ import java.util.Map;
  *
  */
 public class StaticServiceNodeInfo implements ServiceNodeInfo, NodeAddress {
-	private Map<String, String> attributeMap;
-	private final String clusterId;
-	private final String serviceId;
-	private final String nodeId;
+    private Map<String, String> attributeMap;
+    private final String clusterId;
+    private final String serviceId;
+    private final String nodeId;
 
-	private final DefaultNodeId defaultNodeId;
+    private final DefaultNodeId defaultNodeId;
 
-	public StaticServiceNodeInfo(String clusterId, String serviceId, String nodeId, Map<String, String> attributeMap) {
-		if (clusterId == null && serviceId == null && nodeId == null) {
-			throw new IllegalArgumentException("clusterId, serviceId, nodeId cannot be null!");
-		}
+    public StaticServiceNodeInfo(String clusterId, String serviceId, String nodeId,
+            Map<String, String> attributeMap) {
+        if (clusterId == null && serviceId == null && nodeId == null) {
+            throw new IllegalArgumentException("clusterId, serviceId, nodeId cannot be null!");
+        }
 
-		this.clusterId = clusterId;
-		this.serviceId = serviceId;
-		this.nodeId = nodeId;
+        this.clusterId = clusterId;
+        this.serviceId = serviceId;
+        this.nodeId = nodeId;
 
-		try {
-			defaultNodeId = JacksonUtil.getObjectMapper().readValue(nodeId, DefaultNodeId.class);
+        try {
+            defaultNodeId = JacksonUtil.getObjectMapper().readValue(nodeId, DefaultNodeId.class);
 
-			if (attributeMap != null) {
-				this.attributeMap = Collections.unmodifiableMap(attributeMap);
-			} else {
-				this.attributeMap = Collections.EMPTY_MAP;
-			}
-		} catch (Exception e) {
-			throw new ReignException("" + e, e);
-		}
-	}
+            if (attributeMap != null) {
+                this.attributeMap = Collections.unmodifiableMap(attributeMap);
+            } else {
+                this.attributeMap = Collections.EMPTY_MAP;
+            }
+        } catch (Exception e) {
+            throw new ReignException("" + e, e);
+        }
+    }
 
-	@Override
-	public Object getAttribute(String key) {
-		return attributeMap.get(key);
-	}
+    @Override
+    public Object getAttribute(String key) {
+        return attributeMap.get(key);
+    }
 
-	@Override
-	public Map<String, String> getAttributeMap() {
-		return attributeMap;
-	}
+    @Override
+    public Map<String, String> getAttributeMap() {
+        return attributeMap;
+    }
 
-	@Override
-	public String getClusterId() {
-		return clusterId;
-	}
+    @Override
+    public String getClusterId() {
+        return clusterId;
+    }
 
-	@Override
-	public String getServiceId() {
-		return serviceId;
-	}
+    @Override
+    public String getServiceId() {
+        return serviceId;
+    }
 
-	@Override
-	public String getNodeId() {
-		return nodeId;
-	}
+    @Override
+    public String getNodeId() {
+        return nodeId;
+    }
 
-	@Override
-	public String getProcessId() {
-		return defaultNodeId.getProcessId();
-	}
+    @Override
+    public Integer getPort() {
+        return defaultNodeId.getPort();
+    }
 
-	@Override
-	public String getIpAddress() {
-		return defaultNodeId.getIpAddress();
-	}
+    @Override
+    public String getProcessId() {
+        return defaultNodeId.getProcessId();
+    }
 
-	@Override
-	public String getHost() {
-		return defaultNodeId.getHost();
-	}
+    @Override
+    public String getIpAddress() {
+        return defaultNodeId.getIpAddress();
+    }
 
-	@Override
-	public Integer getMessagingPort() {
-		return defaultNodeId.getMessagingPort();
-	}
+    @Override
+    public String getHost() {
+        return defaultNodeId.getHost();
+    }
+
+    @Override
+    public Integer getMessagingPort() {
+        return defaultNodeId.getMessagingPort();
+    }
 
 }
