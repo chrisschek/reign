ee1be2f9d03781be3d6a327f8577eea6baa3e4d6
Yen Pai <ypai@kompany.org>
2014-12-07 22:08:59 -0800

    cleaned up dead code

--- src/main/java/io/reign/DefaultNodeIdProvider.java
+++ src/main/java/io/reign/DefaultNodeIdProvider.java
@@ -29,146 +29,139 @@ import org.codehaus.jackson.annotate.JsonPropertyOrder;
  */
 public class DefaultNodeIdProvider implements NodeIdProvider {
 
-	private String processId;
-	private String host;
-	private String ipAddress;
+    private String processId;
+    private String host;
+    private String ipAddress;
 
-	private final Integer messagingPort;
+    private final Integer messagingPort;
 
-	public DefaultNodeIdProvider() {
-		this(null);
-	}
+    public DefaultNodeIdProvider() {
+        this(null);
+    }
 
-	public DefaultNodeIdProvider(Integer messagingPort) {
-		this.messagingPort = messagingPort;
+    public DefaultNodeIdProvider(Integer messagingPort) {
+        this.messagingPort = messagingPort;
 
-		// get pid
-		processId = IdUtil.getProcessId();
+        // get pid
+        processId = IdUtil.getProcessId();
 
-		// try to get hostname and ip address
-		host = IdUtil.getHostname();
-		ipAddress = IdUtil.getIpAddress();
+        // try to get hostname and ip address
+        host = IdUtil.getHostname();
+        ipAddress = IdUtil.getIpAddress();
 
-		// fill in unknown values
-		if (processId == null) {
-			processId = "";
-		}
-		if (host == null) {
-			host = "";
-		}
-		if (ipAddress == null) {
-			ipAddress = "";
-		}
-	}
+        // fill in unknown values
+        if (processId == null) {
+            processId = "";
+        }
+        if (host == null) {
+            host = "";
+        }
+        if (ipAddress == null) {
+            ipAddress = "";
+        }
+    }
 
-	@Override
-	public String get() {
-		return new DefaultNodeId(processId, ipAddress, host, messagingPort).toString();
-	}
+    @Override
+    public String get() {
+        return new DefaultNodeId(processId, ipAddress, host, messagingPort).toString();
+    }
 
-	// @Override
-	// public ZkNodeId forZk() {
-	// return new ZkNodeId((new DefaultNodeId(processId, ipAddress, host, messagingPort)).toString(), null);
-	// }
+    /**
+     * 
+     * @author ypai
+     * 
+     */
+    @JsonPropertyOrder({ "h", "ip", "p", "mp", "pid" })
+    public static class DefaultNodeId {
 
-	// @Override
-	// public CanonicalId get(Integer port) {
-	// CanonicalId id = new DefaultCanonicalId(processId, ipAddress, host, port, messagingPort);
-	// return id;
-	// }
+        /**
+         * process ID
+         */
+        @JsonProperty("pid")
+        private String processId;
 
-	// @Override
-	// public NodeId fromZk(ZkNodeId zkNodeId) {
-	// try {
-	// return JacksonUtil.getObjectMapper().readValue(zkNodeId.getPathToken(),
-	// new TypeReference<DefaultNodeId>() {
-	// });
-	// } catch (Exception e) {
-	// throw new IllegalArgumentException("Could not parse '" + zkNodeId.getPathToken() + "':  " + e, e);
-	// }
-	// }
+        /**
+         * IP address of node
+         */
+        @JsonProperty("ip")
+        private String ipAddress;
 
-	/**
-	 * 
-	 * @author ypai
-	 * 
-	 */
-	@JsonPropertyOrder({ "h", "ip", "mp", "pid" })
-	public static class DefaultNodeId {
+        /**
+         * host name of node
+         */
+        @JsonProperty("h")
+        private String host;
 
-		/**
-		 * process ID
-		 */
-		@JsonProperty("pid")
-		private String processId;
+        /** the messaging port for the framework */
+        @JsonProperty("mp")
+        private Integer messagingPort;
 
-		/**
-		 * IP address of node
-		 */
-		@JsonProperty("ip")
-		private String ipAddress;
+        /** the port for the service */
+        @JsonProperty("p")
+        private Integer port = null;
 
-		/**
-		 * host name of node
-		 */
-		@JsonProperty("h")
-		private String host;
+        public DefaultNodeId() {
 
-		/** the messaging port for the framework */
-		@JsonProperty("mp")
-		private Integer messagingPort;
+        }
 
-		public DefaultNodeId() {
+        public DefaultNodeId(String processId, String ipAddress, String host, Integer messagingPort) {
+            this.processId = processId;
+            this.ipAddress = ipAddress;
+            this.host = host;
+            this.messagingPort = messagingPort;
+        }
 
-		}
+        public DefaultNodeId(String processId, String ipAddress, String host, Integer messagingPort, Integer port) {
+            this.processId = processId;
+            this.ipAddress = ipAddress;
+            this.host = host;
+            this.port = port;
+            this.messagingPort = messagingPort;
+        }
 
-		public DefaultNodeId(String processId, String ipAddress, String host, Integer messagingPort) {
-			this.processId = processId;
-			this.ipAddress = ipAddress;
-			this.host = host;
-			// this.port = port;
-			this.messagingPort = messagingPort;
-		}
+        public String getProcessId() {
+            return processId;
+        }
 
-		public String getProcessId() {
-			return processId;
-		}
+        public String getIpAddress() {
+            return ipAddress;
+        }
 
-		public String getIpAddress() {
-			return ipAddress;
-		}
+        public String getHost() {
+            return host;
+        }
 
-		public String getHost() {
-			return host;
-		}
+        public Integer getPort() {
+            return port;
+        }
 
-		public Integer getMessagingPort() {
-			return messagingPort;
-		}
+        public Integer getMessagingPort() {
+            return messagingPort;
+        }
 
-		@Override
-		public String toString() {
-			try {
-				return JacksonUtil.getObjectMapper().writeValueAsString(this).replace('"', '\'');
-			} catch (Exception e) {
-				throw new IllegalArgumentException(e);
-			}
-		}
+        @Override
+        public String toString() {
+            try {
+                return JacksonUtil.getObjectMapper().writeValueAsString(this).replace('"', '\'');
+            } catch (Exception e) {
+                throw new IllegalArgumentException(e);
+            }
+        }
 
-		@Override
-		public boolean equals(Object in) {
-			if (in == null || !(in instanceof DefaultNodeId)) {
-				return false;
-			}
+        @Override
+        public boolean equals(Object in) {
+            if (in == null || !(in instanceof DefaultNodeId)) {
+                return false;
+            }
 
-			return in.toString().equals(this.toString());
-		}
+            return in.toString().equals(this.toString());
+        }
 
-		@Override
-		public int hashCode() {
-			return this.toString().hashCode();
-		}
+        @Override
+        public int hashCode() {
+            return this.toString().hashCode();
+        }
 
-	}
+    }
 
 }
