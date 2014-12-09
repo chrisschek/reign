5273b11c55bc5bf4c28dc030a5bd46d3080a4772
Yen Pai <ypai@kompany.org>
2014-12-08 23:24:40 -0800

    added higher tolerance for timing-based tests to allow for per execution fluctuations

--- src/test/java/io/reign/metrics/MetricsServiceTest.java
+++ src/test/java/io/reign/metrics/MetricsServiceTest.java
@@ -224,8 +224,8 @@ public class MetricsServiceTest {
         TimerData timer2 = metricsData.getTimer("timer2");
         assertTrue(timer1.getCount() == 1);
         assertTrue(timer2.getCount() == 2);
-        assertTrue("Unexpected value:  +" + timer1.getMax(), Math.floor(timer1.getMax()) - 100 < 5);
-        assertTrue("Unexpected value:  +" + timer2.getMax(), Math.floor(timer2.getMax()) - 200 < 5);
+        assertTrue("Unexpected value:  " + timer1.getMax(), timer1.getMax() < 150);
+        assertTrue("Unexpected value:  " + timer2.getMax(), timer2.getMax() < 250);
 
         // histograms
         HistogramData histo1 = metricsData.getHistogram("histo1");
