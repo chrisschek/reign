5fec529e66987b95c3d7083f460c44a319b1b814
Yen Pai <ypai@kompany.org>
2014-12-09 14:01:47 -0800

    added more test cases

--- src/test/java/io/reign/lease/LeaseServiceTest.java
+++ src/test/java/io/reign/lease/LeaseServiceTest.java
@@ -1,10 +1,12 @@
 package io.reign.lease;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import io.reign.MasterTestSuite;
 import io.reign.Reign;
 import io.reign.presence.PresenceService;
 
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.junit.Before;
@@ -27,18 +29,26 @@ public class LeaseServiceTest {
 
         presenceService = reign.getService("presence");
         presenceService.announce("clusterLease", "serviceA", true);
+
     }
 
     @Test
     public void testAcquire() throws Exception {
         final long startTimestamp = System.currentTimeMillis();
 
-        final AtomicLong millisToAcquire1 = new AtomicLong(0L);
+        final AtomicBoolean wrongEventHandlerCalled = new AtomicBoolean(false);
+
+        final AtomicLong millisToAcquire1 = new AtomicLong(-1L);
+        final AtomicLong millisToRelease1 = new AtomicLong(-1L);
         leaseService.acquire("clusterLease", "testAcquire", 2, 10000).subscribe(new LeaseEventSubscriber() {
             @Override
             public void acquired(Lease lease) {
                 millisToAcquire1.set(System.currentTimeMillis() - startTimestamp);
 
+                synchronized (millisToAcquire1) {
+                    millisToAcquire1.notifyAll();
+                }
+
                 try {
                     Thread.sleep(1000);
                 } catch (InterruptedException e) {
@@ -47,14 +57,45 @@ public class LeaseServiceTest {
 
                 lease.release().subscribe();
             }
+
+            @Override
+            public void renewed(Lease lease) {
+                wrongEventHandlerCalled.set(true);
+            }
+
+            @Override
+            public void released(Lease lease) {
+                millisToRelease1.set(System.currentTimeMillis() - startTimestamp);
+            }
+
+            @Override
+            public void revoked(Lease lease) {
+                wrongEventHandlerCalled.set(true);
+            }
+
+            @Override
+            public void error(Throwable e) {
+                wrongEventHandlerCalled.set(true);
+            }
+
         });
 
-        final AtomicLong millisToAcquire2 = new AtomicLong(0L);
+        while (millisToAcquire1.get() < 0) {
+            synchronized (millisToAcquire1) {
+                millisToAcquire1.wait(2000);
+            }
+        }
+
+        final AtomicLong millisToAcquire2 = new AtomicLong(-1L);
         leaseService.acquire("clusterLease", "testAcquire", 2, 10000).subscribe(new LeaseEventSubscriber() {
             @Override
             public void acquired(Lease lease) {
                 millisToAcquire2.set(System.currentTimeMillis() - startTimestamp);
 
+                synchronized (millisToAcquire2) {
+                    millisToAcquire2.notifyAll();
+                }
+
                 try {
                     Thread.sleep(1000);
                 } catch (InterruptedException e) {
@@ -65,7 +106,13 @@ public class LeaseServiceTest {
             }
         });
 
-        final AtomicLong millisToAcquire3 = new AtomicLong(0L);
+        while (millisToAcquire2.get() < 0) {
+            synchronized (millisToAcquire2) {
+                millisToAcquire2.wait(2000);
+            }
+        }
+
+        final AtomicLong millisToAcquire3 = new AtomicLong(-1L);
         leaseService.acquire("clusterLease", "testAcquire", 2, 10000).subscribe(new LeaseEventSubscriber() {
             @Override
             public void acquired(Lease lease) {
@@ -79,15 +126,59 @@ public class LeaseServiceTest {
                     millisToAcquire3.notifyAll();
                 }
             }
+
         });
 
-        synchronized (millisToAcquire3) {
-            millisToAcquire3.wait(20000);
+        while (millisToAcquire3.get() < 0) {
+            synchronized (millisToAcquire3) {
+                millisToAcquire3.wait(2000);
+            }
         }
 
+        assertFalse(wrongEventHandlerCalled.get());
+
+        assertTrue(millisToRelease1.get() > 0);
         assertTrue(millisToAcquire1.get() < millisToAcquire3.get());
         assertTrue(millisToAcquire2.get() < millisToAcquire3.get());
         assertTrue(millisToAcquire3.get() >= 1000);
+    }
+
+    @Test
+    public void testRevocation() throws Exception {
+        final long startTimestamp = System.currentTimeMillis();
+
+        final AtomicLong millisToAcquire1 = new AtomicLong(-1L);
+        final AtomicLong millisToRevoke1 = new AtomicLong(-1L);
+        leaseService.acquire("clusterLease", "testRevocation", 1, 5000).subscribe(new LeaseEventSubscriber() {
+            @Override
+            public void acquired(Lease lease) {
+                logger.debug("testRevocation():  ACQUIRED");
+                millisToAcquire1.set(System.currentTimeMillis() - startTimestamp);
+
+                try {
+                    Thread.sleep(20000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void revoked(Lease lease) {
+                logger.debug("testRevocation():  REVOKED");
+                millisToRevoke1.set(System.currentTimeMillis() - startTimestamp);
+
+                synchronized (millisToRevoke1) {
+                    millisToRevoke1.notifyAll();
+                }
+            }
+        });
+
+        while (millisToRevoke1.get() < 0L) {
+            Thread.sleep(1000);
+        }
+
+        assertTrue("millisToAcquire should be less than millisToRevoke", millisToAcquire1.get() < millisToRevoke1.get());
+        assertTrue("millisToRevoke should be >= lease duration", millisToRevoke1.get() >= 5000);
     }
 
 }
