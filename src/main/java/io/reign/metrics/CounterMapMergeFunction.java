6b9c2112eda5bbb435ad1c73ba67682b0e4e260f
Michael Xing <michael.xing@tubemogul.com>
2015-01-15 10:13:08 -0800

    changed author

--- src/main/java/io/reign/metrics/CounterMapMergeFunction.java
+++ src/main/java/io/reign/metrics/CounterMapMergeFunction.java
@@ -1,31 +1,27 @@
 package io.reign.metrics;
 
-import java.util.ArrayList;
-import java.util.Dictionary;
 import java.util.HashMap;
-import java.util.Hashtable;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
-import com.codahale.metrics.Counter;
 import com.google.common.collect.Ordering;
 import com.google.common.primitives.Ints;
 
 /**
  * 
- * @author ypai
+ * @author mxing
  *
  */
 public class CounterMapMergeFunction implements MergeFunction<GaugeData<Map<String, Integer>>> {
-        
+
     private int maxCounters;
-    
+
     public CounterMapMergeFunction() {
         this.maxCounters = 10;
     }
-    
+
     public CounterMapMergeFunction(int maxCounters) {
         this.maxCounters = maxCounters;
     }
@@ -46,7 +42,7 @@ public class CounterMapMergeFunction implements MergeFunction<GaugeData<Map<Stri
                 }
             }
         }
-        
+
         // sort
         Ordering<Map.Entry<String, Integer>> counterOrdering = new Ordering<Map.Entry<String, Integer>>() {
             @Override
@@ -54,15 +50,15 @@ public class CounterMapMergeFunction implements MergeFunction<GaugeData<Map<Stri
                 return Ints.compare(left.getValue(), right.getValue());
             }
         };
-        
+
         Map<String, Integer> topCounters = new LinkedHashMap<String, Integer>();
         for (Map.Entry<String, Integer> entry : counterOrdering.greatestOf(mergedCounters.entrySet(), maxCounters)) {
             topCounters.put(entry.getKey(), entry.getValue());
         }
-        
-        GaugeData<Map<String, Integer>> mergedData = new GaugeData<Map<String,Integer>>();
+
+        GaugeData<Map<String, Integer>> mergedData = new GaugeData<Map<String, Integer>>();
         mergedData.setValue(topCounters);
         return mergedData;
     }
-    
+
 }
